1eba36248e7d66a6632fba39c2900e53283ee933
Desmond Mullen <dsm@busylittlestudios.com>
2019-04-01 20:55:56 -0400

    updated Sessions schema

--- db/heroku schema for sessions.sql
+++ db/heroku schema for sessions.sql
@@ -2,13 +2,11 @@ USE `mp1wuwmz3yjsrqmz`;
 
 CREATE TABLE Sessions
 (
-    sid varchar
-(255),
+    sid varchar (255),
     userId varchar (255),
     expires datetime,
     data text,
     createdAt datetime,
     updatedAt datetime,
-    primary key
-    (sid)
+    primary key (sid)
 );
